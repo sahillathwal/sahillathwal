2d40517390f17254a6797ce3df5b84635a35ab1a
Sahil Lathwal <135765962+sahillathwal@users.noreply.github.com>
2024-03-15 00:53:15 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,52 @@
+import random
+import json
+import requests
+import os
+
+# Category List for Quotes
+category = ['inspirational', 'attitude', 'dreams',
+			'experience', 'intelligence', 'leadership', 'success']
+api_url = 'https://api.api-ninjas.com/v1/\
+quotes?category={}'.format(category[random.randint(0, 6)])
+
+# Enter your API Key Here
+response = requests.get(api_url, headers=
+						{'X-Api-Key': 'tSr006WeNC+nQNxQ3+YMvw==L9aQ1415loq5B0WZ'})
+
+if response.status_code == requests.codes.ok:
+	quote = response.text
+	# Load the response into a json object
+	quotes = json.loads(quote)
+	q = quotes[0]['quote']
+	# In case of receiving multiple quotes,
+	# we will pick the first one
+	mainQuote = q.split('.')[0]
+else:
+	print("Error:", response.status_code, response.text)
+
+
+# Reading the readme file
+with open("README.md", mode="r", encoding="utf8") as f:
+	readmeText = f.read()
+
+# Finding the tag where the quote is to be replaced
+openingTag = "<h3 quote"
+closingTag = "</h3 quote"
+
+startIndex = readmeText.index(openingTag)
+endIndex = readmeText.index(closingTag)
+
+quoteMarkdown = "<h3 quote align='center'>" + mainQuote + "." + "</h3 quote>"
+
+content = readmeText[startIndex +
+					len(openingTag): endIndex]
+newContent = (
+	readmeText[:startIndex]
+	+ quoteMarkdown
+	+ readmeText[endIndex + len(closingTag) + 1:]
+)
+
+# Writing new Quote into readme file
+readme_file = open("README.md",
+				mode="w", encoding="utf8")
+readme_file.write(newContent)
